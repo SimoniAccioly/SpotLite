5e77d94419c7d3b3fdb6c20ad8ca7fb78ee8b79e
SimoniAccioly <67556668+SimoniAccioly@users.noreply.github.com>
2024-03-23 20:40:18 -0300

    Subindo mapper

---
+++ SpotLite.Application/Conta/Dto/CartaoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotLite.Application.Conta.Dto
+{
+    public class CartaoDto
+    {
+        public Guid Id { get; set; }
+        public Boolean Ativo { get; set; }
+        public String Numero { get; set; }
+        public DateTime Validade { get; set; }
+        public int CVV { get; set; }
+        public Decimal Limite { get; set; }
+    }
+}
---
+++ SpotLite.Application/Conta/Profile/UsuarioProfile.cs
@@ -0,0 +1,35 @@
+﻿using SpotLite.Application.Conta.Dto;
+using SpotLite.Domain.Conta.Agreggates;
+using SpotLite.Domain.Financeiro.Agreggates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotLite.Application.Conta.Profile
+{
+    public class UsuarioProfile : AutoMapper.Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<UsuarioDto, Usuario>();
+
+            CreateMap<Usuario, UsuarioDto>()
+            .AfterMap((s, d) =>
+            {
+                var plano = s.Assinaturas?.FirstOrDefault(a => a.Ativo)?.Plano;
+
+                if (plano != null)
+                    d.PlanoId = plano.Id;
+
+                d.Senha = "xxxxxxxxx";
+
+            });
+
+            CreateMap<CartaoDto, Cartao>()
+                .ForPath(x => x.Limite.Valor, m => m.MapFrom(f => f.Limite))
+                .ReverseMap();
+        }
+    }
+}
